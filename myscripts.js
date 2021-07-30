407a0bce1335cff49240e91bbad1f2fac0f584c5
Tranzillo Francesco <f.tranzillo@reply.it>
2021-07-30 18:38:57 +0200

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -4,9 +4,9 @@ function myNavFunc(){
     if( (navigator.platform.indexOf("iPhone") != -1) 
         || (navigator.platform.indexOf("iPod") != -1)
         || (navigator.platform.indexOf("iPad") != -1))
-         window.open("maps://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=[15.189870759238746],[40.877462478407395]");
+         window.open("maps://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=40.877462478407395,15.189870759238746");
     else
-         window.open("https://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=[40.877462478407395],[15.189870759238746]");
+         window.open("https://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=40.877462478407395,15.189870759238746");
 }
 
 
